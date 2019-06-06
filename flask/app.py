4dd5696e106c0287c5ea5b87084b30ab21d9d017
TurtlePig <hello@gmail.com>
2019-06-06 11:46:36 -0300

    arrumado falha no 'login' persistente, o usuário precisava ser colocado em uma lista

--- flask/app.py
+++ flask/app.py
@@ -1,26 +1,32 @@
 from flask import Flask, render_template, request, redirect
 
-app = Flask("__name__")
+global_user = ["Default"]
+
+app = Flask(__name__)
 app.secret_key = 'senha'
 msg = ["ola","hellow"]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+	return render_template("home.html")
 
 @app.route("/send")
 def send():
-    m = request.args.get("msg")
-    msg.append(m)
-    return redirect("/")
+
+	m = request.args.get("msg")
+	msg.append(m)
+	print(global_user)
+	return render_template("chat.html", user = global_user, key = "duh")
 
 @app.route("/entry_room")
 def entry_room():
-    user = request.args.get("user")
-    return render_template("chat.html", user = user)
+	global_user[0] = request.args.get("user")
+	return render_template("chat.html", user = global_user, key = "duh")
 
 @app.route("/msg_box")
 def chat():
-    return render_template("msg_box.html", msg = msg, user = request.args.get('user'))
+	return render_template("msg_box.html", msg = msg, user = global_user[0])
 
+if __name__ == "__main__":
 
-app.run(debug = True)
+	app.run(debug = True)
